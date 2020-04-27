321009d41e37ca7d05ff083a0b6acbd1443e65bc
dogus ural <dogus@riddleandcode.com>
2020-04-27 20:43:09 +0200

    better naming

--- ecc.py
+++ ecc.py
@@ -23,39 +23,39 @@ def createCTypeArrayfromKeyPair(keyPair):
     key_str += '}'
     return key_str
 
-class NISTP_Pair:
+class NISTP_Pair_Ops:
 
     key = None
 
     @staticmethod
     def generate_key(file,curve='P-256'):
-        NISTP_Pair.key = (ECC.generate(curve=curve))
-        NISTP_Pair.save_key(file)
+        NISTP_Pair_Ops.key = (ECC.generate(curve=curve))
+        NISTP_Pair_Ops.save_key(file)
     @staticmethod
     def save_key(filename='myprivatekey.pem'):
         f = open(filename,'wt')
-        f.write(NISTP_Pair.key.export_key(format='PEM'))
+        f.write(NISTP_Pair_Ops.key.export_key(format='PEM'))
         f.close()
     @staticmethod
     def read_key():
         f = open('myprivatekey.pem','rt')
-        NISTP_Pair.key = ECC.import_key(f.read())
+        NISTP_Pair_Ops.key = ECC.import_key(f.read())
         f.close()
-        return NISTP_Pair.key
+        return NISTP_Pair_Ops.key
 
 println("")
 
-NISTP_Pair.generate_key('spareprivatekey.pem')
-whole_key = NISTP_Pair.read_key()
-pub_key = whole_key.pointQ
-sec_key = whole_key.d
-println(createCTypeArrayfromKeyPair(whole_key.export_key(format='DER').hex()))
+NISTP_Pair_Ops.generate_key('spareprivatekey.pem')
+nist_p_pair = NISTP_Pair_Ops.read_key()
+pub_key = nist_p_pair.pointQ
+sec_key = nist_p_pair.d
+println(createCTypeArrayfromKeyPair(nist_p_pair.export_key(format='DER').hex()))
 println(createCTypeArrayfromKeyPair(point_to_secret_key_bytes(sec_key).hex()))
 println(createCTypeArrayfromKeyPair(point_to_public_key_bytes(pub_key).hex()))
 
 h = SHA256.new()
 digest = b'Hello'
 h.update(digest)
-signer = DSS.new(whole_key, 'fips-186-3',encoding='der')
+signer = DSS.new(nist_p_pair, 'fips-186-3',encoding='der')
 signature = signer.sign(h)
 println(createCTypeArrayfromKeyPair(signature.hex()))
